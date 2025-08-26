23d9a8e5e6169e22bc50b381f2804400cebb66f7
Guilherme Aguiar <202311190034@ifba.edu.br>
2025-08-26 16:52:20 -0300

    feat: adição de funções para server side http

--- Controle_XYZ.c
+++ Controle_XYZ.c
@@ -19,6 +19,15 @@
 
 //---------------------------------------FUNÇÕES---------------------------------------
 
+static void send_next_chunk(struct tcp_pcb *tpcb, struct http_state *hs);
+
+static err_t http_sent(void *arg, struct tcp_pcb *tpcb, u16_t len);
+
+static err_t http_recv(void *arg, struct tcp_pcb *tpcb, struct pbuf *p, err_t err);
+
+static err_t connection_callback(void *arg, struct tcp_pcb *newpcb, err_t err);
+
+static void start_http_server(void);
 
 //----------------------------------------TASKS----------------------------------------
 
@@ -33,3 +42,132 @@ int main()
 }
 
 //---------------------------------DECLARAÇÃO DAS FUNÇÕES-----------------------------
+
+#define CHUNK_SIZE 1024
+
+// Função para enviar o próximo pedaço de resposta se houver espaço na janela
+static void send_next_chunk(struct tcp_pcb *tpcb, struct http_state *hs)
+{
+    if (hs->offset >= hs->len) return;
+    size_t remaining = hs->len - hs->offset;
+    size_t to_send = remaining > CHUNK_SIZE ? CHUNK_SIZE : remaining;
+    if (tcp_sndbuf(tpcb) < to_send) return; // Aguardar janela
+    err_t err = tcp_write(tpcb, hs->response + hs->offset, to_send, TCP_WRITE_FLAG_COPY);
+    if (err == ERR_OK)
+    {
+        hs->offset += to_send;
+        tcp_output(tpcb);
+    }
+    else if (err != ERR_MEM)
+    {
+        // log erro irreversível
+        printf("tcp_write fatal: %d\n", err);
+    }
+}
+
+// Função de callback para enviar dados HTTP
+static err_t http_sent(void *arg, struct tcp_pcb *tpcb, u16_t len)
+{
+    struct http_state *hs = (struct http_state *)arg;
+    hs->sent += len;
+    if (hs->sent >= hs->len)
+    {
+        tcp_close(tpcb);
+        free(hs);
+    }
+    else
+    {
+        send_next_chunk(tpcb, hs);
+    }
+    return ERR_OK;
+}
+
+// Função de callback para receber dados HTTP
+static err_t http_recv(void *arg, struct tcp_pcb *tpcb, struct pbuf *p, err_t err)
+{
+    if (!p)
+    {
+        tcp_close(tpcb);
+        return ERR_OK;
+    }
+    tcp_recved(tpcb, p->tot_len);
+
+    char *req = (char *)p->payload;
+    struct http_state *hs = malloc(sizeof(struct http_state));
+    if (!hs)
+    {
+        pbuf_free(p);
+        tcp_close(tpcb);
+        return ERR_MEM;
+    }
+    hs->sent = 0;
+    hs->offset = 0;
+
+    // Endpoint JSON para /web
+    if (strstr(req, "GET /web"))
+    {
+        char json[256];
+        int jsonlen = snprintf(json, sizeof(json),
+            "{\""); //Conteúdo do JSON
+        hs->len = snprintf(hs->response, sizeof(hs->response),
+            "HTTP/1.1 200 OK\r\n"
+            "Content-Type: application/json\r\n"
+            "Content-Length: %d\r\n"
+            "Connection: close\r\n"
+            "\r\n"
+            "%s",
+            jsonlen, json);
+        tcp_arg(tpcb, hs);
+        tcp_sent(tpcb, http_sent);
+        send_next_chunk(tpcb, hs);
+        pbuf_free(p);
+        return ERR_OK;
+    }
+
+    else
+    {
+        hs->len = snprintf(hs->response, sizeof(hs->response),
+                           "HTTP/1.1 200 OK\r\n"
+                           "Content-Type: text/html\r\n"
+                           "Content-Length: %d\r\n"
+                           "Connection: close\r\n"
+                           "\r\n"
+                           "%s",
+                           (int)strlen(HTML_BODY), HTML_BODY);
+    }
+
+    tcp_arg(tpcb, hs);
+    tcp_sent(tpcb, http_sent);
+
+    // inicia envio por fatias
+    send_next_chunk(tpcb, hs);
+
+    pbuf_free(p);
+    return ERR_OK;
+}
+
+// Função de callback para aceitar novas conexões TCP
+static err_t connection_callback(void *arg, struct tcp_pcb *newpcb, err_t err)
+{
+    tcp_recv(newpcb, http_recv);
+    return ERR_OK;
+}
+
+// Função para iniciar o servidor HTTP
+static void start_http_server(void)
+{
+    struct tcp_pcb *pcb = tcp_new();
+    if (!pcb)
+    {
+        printf("Erro ao criar PCB TCP\n");
+        return;
+    }
+    if (tcp_bind(pcb, IP_ADDR_ANY, 80) != ERR_OK)
+    {
+        printf("Erro ao ligar o servidor na porta 80\n");
+        return;
+    }
+    pcb = tcp_listen(pcb);
+    tcp_accept(pcb, connection_callback);
+    printf("Servidor HTTP rodando na porta 80...\n");
+}
